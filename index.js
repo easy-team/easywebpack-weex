3c268d8a1dd3b5d47c0c532117e7b0b085e81807
hubcarl <hubcarl@126.com>
2017-07-25 19:58:30 +0800

    feat:support cli

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ exports.EasyWebpack = EasyWebpack;
 exports.WebpackWeexBuilder = require('./lib/weex');
 exports.WebpackWebBuilder = require('./lib/web');
 Object.assign(exports, EasyWebpack);
+
+exports.getWebpackConfig = config => {
+  return EasyWebpack.WebpackBuilder.getWebpackConfig(config, [exports.WebpackWeexBuilder, exports.WebpackWebBuilder]);
+};
